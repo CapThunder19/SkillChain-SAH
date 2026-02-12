75f0d52e472c54c7db47bc12954eccd59543fa89
Anirudh <anirudhpatwal19@gmail.com>
2026-02-12 20:30:22 +0530

    change

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  webpack: (config) => {
-    // Handle JSON files
-    config.resolve.extensions.push('.json');
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
